cf3c089b2498f876c055e44b5e43bcfe42931ede
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2018-01-05 13:02:23 +0100

    Update test_integrals1.py

--- math/tests/test_integrals1.py
+++ math/tests/test_integrals1.py
@@ -1,6 +1,6 @@
 
-import integrals as int
+import integrals 
 
 t_dom1 = np.arange(10)
 def f_x2(x): return x^2
-print("Integral Result = " + str(int.do_integrate_1d(f_x2, t_dom1)))
+print("Integral Result = " + str(integrals.do_integrate_1d(f_x2, t_dom1)))
