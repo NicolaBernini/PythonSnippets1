c626715d6c485eb3595553c5b07fe155ba011a1f
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2018-01-05 13:27:03 +0100

    Update integrals.py

--- math/src/integrals.py
+++ math/src/integrals.py
@@ -1,9 +1,12 @@
 
+import numpy as np 
+import measure 
+
 # The 1D Integral 
 def do_integrate_1d(in_f, in_domain, in_measure=None): 
     if(in_measure is None): 
         # Unnormalized Measure 
-        in_measure = np.full(len(in_domain), 1)
+        in_measure = measure.do_measure_unnormalized(in_domain)
     temp=0
     for i in range(0, len(in_domain)): 
         temp += in_f(in_domain[i])*in_measure[i]
@@ -21,6 +24,6 @@ def do_integrate_1d(in_f, in_domain, in_measure=None):
 def do_expected_value(in_f, in_domain, in_measure=None): 
     if(in_measure is None): 
         # Normalized Measure 
-        in_measure = np.full(len(in_domain), 1/len(in_domain))
+        in_measure = measure.do_measure_normalized(in_domain)
     return do_integrate_1d(in_f, in_domain, in_measure)
 
