91e3df80c178a843938c4eb226f48d6f930bdd04
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2018-01-05 13:45:59 +0100

    Update information_theory.py

--- math/src/information_theory.py
+++ math/src/information_theory.py
@@ -2,7 +2,10 @@
 import numpy as np 
 import integrals 
 
+############## MEASURES ###############
 # Kullback Leibler Divergence 
 def do_kl_compute(in_p, in_q, in_domain, in_prior=None): 
     return do_expected_value( lambda x: in_p(x)*np.log(in_p(x)/in_q(x)), in_domain, in_prior )
+
+
 
