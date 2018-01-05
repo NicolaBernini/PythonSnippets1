678632fc23a8f493d108e9be37248701603a78c6
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2018-01-05 13:43:36 +0100

    Update integrals.py

--- math/src/integrals.py
+++ math/src/integrals.py
@@ -6,10 +6,12 @@ import measure
 def do_integrate_1d(in_f, in_domain, in_measure=None): 
     if(in_measure is None): 
         # Unnormalized Measure 
-        in_measure = measure.do_measure_unnormalized(in_domain)
+        in_measure = do_measure_unnormalized(in_domain)
     temp=0
     for i in range(0, len(in_domain)): 
-        temp += in_f(in_domain[i])*in_measure[i]
+        # Discard NaN 
+        if(not np.isnan(in_f(in_domain[i]))): 
+            temp += in_f(in_domain[i])*in_measure[i]
     return temp
 
 
