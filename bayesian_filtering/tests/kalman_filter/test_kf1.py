282dfbe304e282099e07d229f3c5012ea27a4d27
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2018-01-05 14:25:39 +0100

    Update test_kf1.py

--- bayesian_filtering/tests/kalman_filter/test_kf1.py
+++ bayesian_filtering/tests/kalman_filter/test_kf1.py
@@ -16,7 +16,7 @@ t_sigma_obs = np.array([0.5, 0.5, 0.5])
 
 
 # KF Fusion 
-t_mu_updated = kkf1.do_fuse_kalman1(t_mu_pred, t_sigma_pred, t_mu_obs, t_sigma_obs)
+t_mu_updated = kf1.do_fuse_kalman1(t_mu_pred, t_sigma_pred, t_mu_obs, t_sigma_obs)
 
 
 print("Updated Position = " + str(t_mu_updated))
