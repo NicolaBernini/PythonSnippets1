1d3ecb6d72ee8bd817b859cd9060605e91e7d34f
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2018-01-05 14:47:04 +0100

    Update kf1.py

--- bayesian_filtering/src/kalman_filter/kf1.py
+++ bayesian_filtering/src/kalman_filter/kf1.py
@@ -4,6 +4,18 @@ def do_fuse_kalman1(in_mu_pred, in_sigma_pred, in_mu_obs, in_sigma_obs):
     # Residual = Observation - Prediction 
     t_residuals = np.subtract(in_mu_obs, in_mu_pred)
     t_kalman_gains = np.divide(in_sigma_pred, np.add(in_sigma_pred, in_sigma_obs))
+    
+    # Posterior Mean  
     t_mu_out = np.add(in_mu_pred, np.multiply(t_residuals, t_kalman_gains))
-    return t_mu_out
+    
+    t_sigma_out = np.subtract( in_sigma_pred, np.multiply(t_kalman_gains, in_sigma_pred) )
+    return t_mu_out, t_sigma_out
+
+
+
+
+
+
+
+
 
