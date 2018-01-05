d3561f68e45d40f4ce869d960e8a41a7442a5360
Nicola Bernini <NicolaBernini@users.noreply.github.com>
2018-01-05 13:01:16 +0100

    Update integrals.py

--- math/src/integrals.py
+++ math/src/integrals.py
@@ -1,10 +1,26 @@
 
 # The 1D Integral 
 def do_integrate_1d(in_f, in_domain, in_measure=None): 
-    if(in_measure==None): 
-        in_measure = np.full(len(in_domain), 1/len(in_domain))
+    if(in_measure is None): 
+        # Unnormalized Measure 
+        in_measure = np.full(len(in_domain), 1)
     temp=0
     for i in range(0, len(in_domain)): 
         temp += in_f(in_domain[i])*in_measure[i]
     return temp
+
+
+
+
+
+
+
+
+
+# The Expected Value Operator 
+def do_expected_value(in_f, in_domain, in_measure=None): 
+    if(in_measure is None): 
+        # Normalized Measure 
+        in_measure = np.full(len(in_domain), 1/len(in_domain))
+    return do_integrate_1d(in_f, in_domain, in_measure)
 
